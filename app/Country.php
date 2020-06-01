06175f63c550717853f158a690ec2d48af12db62
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-06-01 15:00:55 -0400

    Countries migrate, model & seeder

---
+++ app/Country.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $fillable = ['name', 'iso_code_cca2', 'iso_code_cc3'];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    /**
+     * Country has many Players.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function players()
+    {
+        return $this->hasMany(Player::class);
+    }
+}
---
+++ database/migrations/2020_06_01_182317_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('iso_code_cca2', 2);
+            $table->string('iso_code_cca3', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
