36ce36ee578350510838e7135254a4ee02d70eed
ruthcast <ruthcastellin@gmail.com>
2020-06-17 20:50:54 -0400

    registro de grupos de usuarios

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -6,6 +6,7 @@ use App\ContactGroup;
 use Illuminate\Http\Request;
 use App\Group;
 use App\User;
+use App\GroupUser;
 
 class GroupController extends Controller
 {
@@ -17,6 +18,32 @@ class GroupController extends Controller
     public function create(){
         $users=User::orderBy('name','desc')->get();
         // var_dump($users); die();
-        return view('groups.form-group', ['users'=> $users]);
+        return view('groups.form-group', ['users'=> $users,]);
+    }
+
+    public function edit($group_id){
+        $group=Group::find((int)$group_id);
+        $userGroup=GroupUser::where('group_id', $group_id)->get();
+        $users=User::orderBy('name','desc')->get();
+        return view('groups.edit-group', ['group'=> $group, 'userGroup'=>$userGroup, 'users'=>$users ]);
+    }
+
+    public function store(Request $request){
+        $validation= $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+
+        $grupo = Group::updateOrCreate(
+            ['name' =>  $request->name,
+             'user_id' => auth()->user()->id
+            ]
+        );
+        if($grupo && $request->input('users')){
+            $users=$request->input('users');
+            var_dump($users); die();
+            return 'Hola';
+    
+        }
     }
 }
--- resources/views/groups/edit-group.blade.php
+++ resources/views/groups/edit-group.blade.php
@@ -11,19 +11,18 @@
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Crear grupo de usuarios</h4>
+                            <h4 class="card-title">Editar grupo de usuarios</h4>
                         </div>
                         <div class="card-content">
                             <div class="card-body">
-                                <form action="{{route('group.saved')}}" class="form form-vertical" method="POST">
-                                    @csrf
+                                <form class="form form-vertical">
                                     <div class="form-body">
                                         <div class="row">
                                             <div class="col-12">
                                                 <div class="form-group">
                                                     <label for="first-name-icon">Nombre del grupo</label>
                                                     <div class="position-relative has-icon-left">
-                                                        <input type="text" id="first-name-icon" class="form-control" name="name" placeholder="Nombre del grupo">
+                                                        <input type="text" id="first-name-icon" value="{{$group->name}}" class="form-control" name="name" placeholder="">
                                                         <div class="form-control-position">
                                                             <i class="feather icon-user"></i>
                                                         </div>
@@ -33,7 +32,18 @@
                                             <div class="col-12">
                                                 <div class="form-group">
                                                     <label for="email-id-icon">Usuarios en este grupo</label>
-                                                    <select class="select2 form-control" multiple="multiple" id="" name="users[]">
+                                                    <select class="custom-select form-control" id="" name="users[]">
+                                                        @foreach($userGroup as $user)
+                                                            <option value="{{$user->id}}">{{$user->user->name}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <div class="form-group">
+                                                    <label for="email-id-icon">Añade un usuario a este grupo</label>
+                                                    <select class="custom-select form-control" id="" name="users[]">
                                                         @foreach($users as $user)
                                                             <option value="{{$user->id}}">{{$user->name}}</option>
                                                         @endforeach
@@ -42,7 +52,7 @@
                                             </div>
                                             <div class="col-12">
                                                 <button type="submit" class="btn btn-primary mr-1 mb-1 float-right">Guardar Cambios</button>
-                                                <a href="{{route('group.show')}}" type="button" class="btn btn-outline-warning mr-1 mb-1 float-left">Cancelar</a>
+                                                <a href="{{route('group.show')}}" class="btn btn-outline-warning mr-1 mb-1 float-left">Cancelar</a>
                                             </div>
                                         </div>
                                     </div>
