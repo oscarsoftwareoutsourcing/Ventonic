841fe4cfe258317a4bcd1f24476c0ff1a5a5bb1f
Ernesto Murrugarra <ernestom@softwareoutsourcing.es>
2020-06-30 05:56:44 -0400

    Archivos que pide para poder hacer pull y luego merge

---
+++ public/js/components/15.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[15],{
+
+/***/ "./resources/js/components/NegForm.vue":
+/*!*********************************************!*\
+  !*** ./resources/js/components/NegForm.vue ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/vue-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\laragon\\\\www\\\\Ventonic\\\\resources\\\\js\\\\components\\\\NegForm.vue'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9jb21wb25lbnRzL05lZ0Zvcm0udnVlLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./resources/js/components/NegForm.vue\n");
+
+/***/ })
+
+}]);
---
+++ public/js/components/16.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[16],{
+
+/***/ "./resources/js/components/Negotiations.vue":
+/*!**************************************************!*\
+  !*** ./resources/js/components/Negotiations.vue ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/vue-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\laragon\\\\www\\\\Ventonic\\\\resources\\\\js\\\\components\\\\Negotiations.vue'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9jb21wb25lbnRzL05lZ290aWF0aW9ucy52dWUuanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./resources/js/components/Negotiations.vue\n");
+
+/***/ })
+
+}]);
