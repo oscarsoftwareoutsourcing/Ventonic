7d4d765b3d8a22f73e6004b7bb3612895ca5e5ee
Ing. Roldan Vargas <rolvar@softwareoutsourcing.es>
2020-08-11 23:27:57 -0400

    ajustes en iniciales de nombre a mostrar cuando no existe imagen establecida de contacto

--- resources/views/contact/card.blade.php
+++ resources/views/contact/card.blade.php
@@ -6,24 +6,24 @@
                     <div class="row">
                         <div class="col-lg-3 col-md-3 col-sm-3 center">
                             <div class="mx-auto text-center mx-auto">
-                                @if ($contact['image'] != null)
+                                @if ($contact->image != null)
                                     <div class="avatar avatar-xl">
-                                        <img class="img-fluid" src="/{{$contact['image']}}" alt="{{ $contact['fullName'] }}">
+                                        <img class="img-fluid" src="/{{$contact->image}}" alt="{{ $contact->fullName }}">
                                     </div>
                                 @else
                                     <div class="avatar bg-primary mr-1 avatar-lg">
                                         <div class="avatar-content">
-                                            <strong>{{$contact['initials']}}</strong>
+                                            <strong>{{$contact->IntialsName}}</strong>
                                         </div>
                                     </div>
                                 @endif
                             </div>
                         </div>
                         <div class="col-lg-9 col-md-9 col-sm-9" >
-                            <h5>{{ $contact['fullName'] }}</h5>
-                            <p class="card-text  mb-0">{{$contact['type']}}</p>
-                            <span class="card-text">{{$contact['email']}}</span>
-                            <p class="card-text  mb-0">{{$contact['phone']}}</p>
+                            <h5>{{ $contact->fullName }}</h5>
+                            <p class="card-text  mb-0">{{$contact->type}}</p>
+                            <span class="card-text">{{$contact->email}}</span>
+                            <p class="card-text  mb-0">{{$contact->phone}}</p>
                         </div>
                     </div>
                 </div>
