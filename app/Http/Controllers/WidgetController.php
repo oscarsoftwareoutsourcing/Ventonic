281f5ec0770d6ce94f4f12c3dc517a0ec5e0444a
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-08-19 15:47:54 -0400

    fixed in list widgetData 3

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -122,17 +122,17 @@ class WidgetController extends Controller
     public function widgetsData(){
 
         $user_id = Auth::user()->id;
-
+        
         $data = Widget::join('widget_data','widget.id','=','widget_data.widget_id')
-        ->Leftjoin('seller_profiles','seller_profiles.user_id','=','widget.user_id_referred')
         ->join('users','users.id','=','widget.user_id_referred')
+        ->Leftjoin('seller_profiles','seller_profiles.user_id','=','widget.user_id_referred')
         ->selectRaw("widget_data.created_at,
-        widget_data.origin as url,
-        seller_profiles.
-        phone_mobil,
-        users.name,'Call Me' as product")
-        ->where('widget.user_id','=',$user_id)->get();
-        dd($data);
+                    widget_data.origin as url,
+                    seller_profiles.
+                    phone_mobil,
+                    users.name,
+                    'Call Me' as product")
+        ->where('widget.user_id',$user_id)->get();
 
         return view('widget_data.widget-data',['data'=>$data]);
     }
