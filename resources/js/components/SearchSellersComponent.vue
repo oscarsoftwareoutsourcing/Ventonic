13b81a32a98d85ec32814b1c6771382172307bc7
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-09-06 00:02:01 -0400

    ajustes filtros vendedores

--- resources/js/components/SearchSellersComponent.vue
+++ resources/js/components/SearchSellersComponent.vue
@@ -1,163 +1,189 @@
 <template>
-  <div class="row justify-content-center">
-    <div class="col-md-3">
-      <div class="card">
-        <div class="bg-gradient-primary">
-          <div class="card_vetonic-description">
-            <div class="text_vetonic-description1">Filtros</div>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-12 mb-4">
-              <b>Estado de conexión</b>
-              <div class="row">
-                <div class="col-12">
-                  <div class="form-check">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      :value="true"
-                      v-model="status"
-                      :disabled="statusDisconnected"
-                    />
-                    <label class="form-check-label">Conectado</label>
-                  </div>
-                  <div class="form-check">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      :value="true"
-                      v-model="statusDisconnected"
-                      :disabled="status"
-                    />
-                    <label class="form-check-label">Desconectado</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 mb-4" v-for="survey in surveys">
-              <b>{{ survey.name }}</b>
-              <div class="row" v-for="(question, index) in getOptions(survey.options)">
-                <div class="col-12">
-                  <div class="form-check">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      :value="{'question_id': survey.id, 'option_index': index}"
-                      v-model="filters"
-                    />
-                    <label class="form-check-label">{{ question }}</label>
+  <div>
+    <div class="customizer d-none d-md-block">
+      <a class="customizer-close" href="#" @click="closeFilter">
+        <i class="feather icon-x"></i>
+      </a>
+      <!--
+    <a class="customizer-toggle" href="#">
+      <i class="feather icon-settings fa fa-spin fa-fw white"></i>
+    </a>
+      -->
+      <div class="customizer-content p-2 ps ps--active-y">
+        <h4 class="text-uppercase mb-0">Filtros</h4>
+        <small>Negociaciones</small>
+        <hr />
+
+        <div id="lgFiltersBar" class="col-lg-auto d-none d-lg-block filtros">
+          <!-- Filters -->
+
+          <perfect-scrollbar>
+            <div class="row">
+              <div class="col-12 mb-4">
+                <b>Estado de conexión</b>
+                <div class="row">
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="true"
+                        v-model="status"
+                        :disabled="statusDisconnected"
+                      />
+                      <label class="form-check-label">Conectado</label>
+                    </div>
+                    <div class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="true"
+                        v-model="statusDisconnected"
+                        :disabled="status"
+                      />
+                      <label class="form-check-label">Desconectado</label>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+
+            <div class="row">
+              <div class="col-12 mb-4" v-for="survey in surveys">
+                <b>{{ survey.name }}</b>
+                <div class="row" v-for="(question, index) in getOptions(survey.options)">
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        :value="{'question_id': survey.id, 'option_index': index}"
+                        v-model="filters"
+                      />
+                      <label class="form-check-label">{{ question }}</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </perfect-scrollbar>
         </div>
       </div>
     </div>
-    <div class="col-md-9">
-      <div class="row">
-        <div class="col-3">
-          <div class="title-sales my-1">Vendedores</div>
-        </div>
-        <div class="col-9 header-ventonic">
-          <fieldset class="form-group position-relative has-icon-left my-1">
-            <input
-              type="text"
-              class="form-control"
-              id="iconLeft1"
-              placeholder="Buscar por Nombre, Apellido"
-              v-model="searchSeller"
-            />
-            <div class="form-control-position">
-              <i class="ficon feather icon-search"></i>
+
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="row header-ventonic">
+          <div class="col-3">
+            <div class="title-sales my-1">Vendedores</div>
+          </div>
+          <div class="col-7">
+            <fieldset class="form-group position-relative has-icon-left my-1">
+              <input
+                type="text"
+                class="form-control"
+                id="iconLeft1"
+                placeholder="Buscar por Nombre, Apellido"
+                v-model="searchSeller"
+              />
+              <div class="form-control-position">
+                <i class="ficon feather icon-search"></i>
+              </div>
+            </fieldset>
+          </div>
+          <div class="col-2">
+            <div class="mr-auto float-right bookmark-wrapper d-flex align-items-center my-1">
+              <button
+                type="button"
+                class="btn btn-dark btn btn-block waves-effect waves-light"
+                @click="openFilter"
+              >
+                <i class="feather icon-settings fa fa-spin fa-fw white"></i>
+              </button>
             </div>
-          </fieldset>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div v-for="seller in sellers" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-          <div class="card">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-3">
-                  <div class="avatar avatar-xl">
-                    <img
-                      :src="seller.photo"
-                      :alt="seller.name"
-                      class="img-fluid"
-                      v-if="seller.photo"
-                      height="40"
-                      width="40"
-                    />
-                    <img
-                      src="/images/anonymous-user.png"
-                      class="media-object rounded-circle"
-                      :alt="seller.name"
-                      height="40"
-                      width="40"
-                      v-else
-                    />
-                    <div v-if="seller.seller_profile">
-                      <span :class="seller.status ? 'avatar-status-online' :'avatar-status-busy'"></span>
-                    </div>
-                    <div v-else>
-                      <span :class="seller.status ? 'avatar-status-online' :'avatar-status-busy'"></span>
+        <div class="row">
+          <div v-for="seller in sellers" class="col-lg-4 col-md-4 col-sm-16 col-xs-12">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="avatar avatar-xl">
+                      <img
+                        :src="seller.photo"
+                        :alt="seller.name"
+                        class="img-fluid"
+                        v-if="seller.photo"
+                        height="40"
+                        width="40"
+                      />
+                      <img
+                        src="/images/anonymous-user.png"
+                        class="media-object rounded-circle"
+                        :alt="seller.name"
+                        height="40"
+                        width="40"
+                        v-else
+                      />
+                      <div v-if="seller.seller_profile">
+                        <span :class="seller.status ? 'avatar-status-online' :'avatar-status-busy'"></span>
+                      </div>
+                      <div v-else>
+                        <span :class="seller.status ? 'avatar-status-online' :'avatar-status-busy'"></span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div class="col-lg-9 col-md-9 col-sm-9">
-                  <span class="float-right">
+                  <div class="col-lg-9 col-md-9 col-sm-9">
+                    <span class="float-right">
+                      <div v-if="seller.seller_profile">
+                        <button
+                          @click="contactSeller(seller.id)"
+                          :class="seller.status ? 'btn btn-icon btn-icon rounded-circle btn-success mr-1 mb-1 waves-effect waves-light' : 'btn btn-icon btn-icon rounded-circle btn-warning mr-1 mb-1 waves-effect waves-light'"
+                          data-toggle="tooltip"
+                          :title="seller.status ? 'Contactar a este vendedor' : 'Dejar un mensaje al vendedor'"
+                        >
+                          <i
+                            :class="seller.status ? 'ficon feather icon-message-square' : 'ficon feather icon-mail'"
+                          ></i>
+                        </button>
+                        <form
+                          action="/chat"
+                          method="POST"
+                          :id="'chat_'+seller.seller_profile.user_id"
+                        >
+                          <input type="hidden" name="_token" :value="token" />
+                          <input type="hidden" name="user" :value="seller.seller_profile.user_id" />
+                        </form>
+                      </div>
+                      <div v-else>
+                        <button
+                          type="button"
+                          :class="seller.status ? 'btn btn-icon btn-icon rounded-circle btn-success mr-1 mb-1 waves-effect waves-light' : 'btn btn-icon btn-icon rounded-circle btn-warning mr-1 mb-1 waves-effect waves-light'"
+                          data-toggle="tooltip"
+                          :title="seller.status ? 'Contactar a este vendedor' : 'Dejar un mensaje al vendedor'"
+                          @click="contactSeller(seller.id)"
+                        >
+                          <i
+                            :class="seller.status ? 'ficon feather icon-message-square' : 'ficon feather icon-mail'"
+                          ></i>
+                        </button>
+                      </div>
+                    </span>
+
+                    <h3>{{ seller.name }} {{ seller.last_name }}</h3>
+                    <div class="email-sales">{{ seller.email }}</div>
+                    <p class="card-text mb-1">Última Conexión {{ seller.last_login }}</p>
+
                     <div v-if="seller.seller_profile">
-                      <button
-                        @click="contactSeller(seller.id)"
-                        :class="seller.status ? 'btn btn-icon btn-icon rounded-circle btn-success mr-1 mb-1 waves-effect waves-light' : 'btn btn-icon btn-icon rounded-circle btn-warning mr-1 mb-1 waves-effect waves-light'"
-                        data-toggle="tooltip"
-                        :title="seller.status ? 'Contactar a este vendedor' : 'Dejar un mensaje al vendedor'"
-                      >
-                        <i
-                          :class="seller.status ? 'ficon feather icon-message-square' : 'ficon feather icon-mail'"
-                        ></i>
-                      </button>
-                      <form
-                        action="/chat"
-                        method="POST"
-                        :id="'chat_'+seller.seller_profile.user_id"
-                      >
-                        <input type="hidden" name="_token" :value="token" />
-                        <input type="hidden" name="user" :value="seller.seller_profile.user_id" />
-                      </form>
+                      <a
+                        class="byn bg-gradient-primary waves-effect waves-light text-white btn-sm"
+                        :href="'profile/aplicant/'+ seller.id "
+                      >Ver perfil</a>
                     </div>
-                    <div v-else>
-                      <button
-                        type="button"
-                        :class="seller.status ? 'btn btn-icon btn-icon rounded-circle btn-success mr-1 mb-1 waves-effect waves-light' : 'btn btn-icon btn-icon rounded-circle btn-warning mr-1 mb-1 waves-effect waves-light'"
-                        data-toggle="tooltip"
-                        :title="seller.status ? 'Contactar a este vendedor' : 'Dejar un mensaje al vendedor'"
-                        @click="contactSeller(seller.id)"
-                      >
-                        <i
-                          :class="seller.status ? 'ficon feather icon-message-square' : 'ficon feather icon-mail'"
-                        ></i>
-                      </button>
-                    </div>
-                  </span>
-
-                  <h3>{{ seller.name }} {{ seller.last_name }}</h3>
-                  <div class="email-sales">{{ seller.email }}</div>
-                  <p class="card-text mb-1">Última Conexión {{ seller.last_login }}</p>
-
-                  <div v-if="seller.seller_profile">
-                    <a
-                      class="byn bg-gradient-primary waves-effect waves-light text-white btn-sm"
-                      :href="'profile/aplicant/'+ seller.id "
-                    >Ver perfil</a>
+                    <div v-else>Perfil No Disponible</div>
                   </div>
-                  <div v-else>Perfil No Disponible</div>
                 </div>
               </div>
             </div>
@@ -271,6 +297,12 @@ export default {
         .catch((error) => {
           console.error(error);
         });
+    },
+    openFilter: function () {
+      $(".customizer").toggleClass("open");
+    },
+    closeFilter: function () {
+      $(".customizer").removeClass("open");
     },
   },
   mounted() {
