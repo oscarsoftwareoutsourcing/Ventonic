8c1ab0f590b381a6d367476506a6587e528b8ba7
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-10-20 23:56:17 -0400

    videos de ayuda

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -33,17 +33,62 @@
     <div class="content-overlay"></div>
     <div class="header-navbar-shadow"></div>
 
+
+    <div class="content-help">
+        <div class="link-help">
+        <button type="button" class=" btn btn-primary btn-sm waves-effect waves-light"
+        data-toggle="modal"
+        data-target="#primary"
+        id="postularseBtn"
+        >Ver ayuda</button>
+        </div>
+    </div>    
+
     {{-- Sección para chat --}}
     <chat :user="{{ auth()->user() }}" :chat-room-user="{{ json_encode(session('chat_room_user', '')) }}"
           :contacts="{{ auth()->user()->contact()->whereNotNull('email')->get('email') }}"></chat>
 
+          <div class="text-left modal fade" id="primary" tabindex="-1" role="dialog" aria-labelledby="myModalLabel160" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header bg-gradient-primary white">
+                        <h5 class="modal-title" id="myModalLabel160">Chat</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="closeVideo">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="embed-responsive embed-responsive-item embed-responsive-16by9">
+                            <!-- <div v-html="callme.iframe"></div> -->
+                            <video id="videoContainer" width="100%" preload controls>
+                            @if(\Auth::user()->type=="V")
+                                <source src="{{ asset('video/Chat-Perfil-Vendedor.mp4') }}" />
+                                <source src="{{ asset('video/Chat-Perfil-Vendedor.mp4') }}" />
+                                <source src="{{ asset('video/Chat-Perfil-Vendedor.mp4') }}" />
+                            @else
+                                <source src="{{ asset('video/Chat-Perfil-Empresa.mp4') }}" />
+                                <source src="{{ asset('video/Chat-Perfil-Empresa.mp4') }}" />
+                                <source src="{{ asset('video/Chat-Perfil-Empresa.mp4') }}" />
+                            @endif
+                            </video>
+                        </div>
+                </div>
+            </div>
+        </div>
 </div>
 @endsection
 
 @section('extra-js-app')
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script>
+        const boton = document.querySelector("#closeVideo");
+        boton.addEventListener("click", function(evento){
+            const video = document.getElementById("videoContainer");
+            video.pause();
+            return false;
+        });
+    </script>
 @endsection
 
 @section('extra-js')
-<script src="{{ asset('js/scripts/pages/app-chat.js') }}"></script>
+< src="{{ asset('js/scripts/pages/app-chat.js') }}"></>
 @endsection
