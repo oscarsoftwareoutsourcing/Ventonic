79a0e358ebcbf84f226e31094c239f53916fe5fe
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-10-26 22:41:58 -0400

    no mostrar valoracion en empresa

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -346,23 +346,25 @@
 
                                     </div>
                                 @endif
-
-                                <hr class="mt-5 mb-5">
-                                <h6 class="display-6 text-center mt-3 mb-3">Valoraciones</h6>
-                                <div class="alert alert-success alert-request" role="alert"
-                                     style="display:none"></div>
-                                @php
-                                    $user = auth()->user();
-                                @endphp
-                                @if ($user->ratings->isEmpty())
-                                    <rating-request :user="{{ $user }}"></rating-request>
-                                @else
-                                    <rating-show :user="{{ $user }}" :inactive-color="'#10163A'"
-                                                 :active-color="'#0086FA'" :border-width="2"
-                                                 :border-color="'#0086FA'"></rating-show>
-                                    <rating-request :user="{{ $user }}"
-                                                    :has-ratings="{{ ($user->ratings->isEmpty())?'false':'true' }}"></rating-request>
-                                @endif
+                                
+                                @if ($type === 'V')
+                                    <hr class="mt-5 mb-5">
+                                    <h6 class="display-6 text-center mt-3 mb-3">Valoraciones</h6>
+                                    <div class="alert alert-success alert-request" role="alert"
+                                        style="display:none"></div>
+                                    @php
+                                        $user = auth()->user();
+                                    @endphp
+                                    @if ($user->ratings->isEmpty())
+                                        <rating-request :user="{{ $user }}"></rating-request>
+                                    @else
+                                        <rating-show :user="{{ $user }}" :inactive-color="'#10163A'"
+                                                    :active-color="'#0086FA'" :border-width="2"
+                                                    :border-color="'#0086FA'"></rating-show>
+                                        <rating-request :user="{{ $user }}"
+                                                        :has-ratings="{{ ($user->ratings->isEmpty())?'false':'true' }}"></rating-request>
+                                    @endif
+                                 @endif
 
 
                                 @if (!$questions->isEmpty())
